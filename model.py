b2175a681cd03de944d655da0383177b8d75a14c
ManjotSinghJolly <108326133+ManjotSinghJolly@users.noreply.github.com>
2024-05-09 16:25:38 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        # Creating an activation function
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.l1(x)
+        # Applying the activation function
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        out = self.relu(out)
+        out = self.l3(out)
+        # No activation and no softmax
+        return out
